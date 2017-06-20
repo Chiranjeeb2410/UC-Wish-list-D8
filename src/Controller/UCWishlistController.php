a424a2d7d5a136261e462dddf63e2d2e87420b75
chiranjeeb2410 <chiranjeebm2410@gmail.com>
2017-06-20 11:04:29 -0400

    view user wish list option

--- src/Controller/UCWishlistController.php
+++ src/Controller/UCWishlistController.php
@@ -96,5 +96,92 @@ class UCWishlistController extends ControllerBase implements ContainerInjectionI
             '#rows' => $rows
         );
     }
-}
 
+    public function viewWishlist($wid = NULL)
+    {
+        $output = '';
+        $title = '';
+        $wishlist = null;
+        $render = array();
+        $rendered_wishlistview_form = '';
+        $own = false;
+        if (!$own && $wid == uc_wishlist_get_wid()) {
+            //user is viewing their own wishlist so redirect them to the correct route
+            return $this->redirect('uc_wishlist.wishlist');
+        }
+
+        // Attempt to load the wish list.
+        $wishlist = uc_wishlist_load($wid);
+
+        // Handle a non-existent wish list.
+        if (!$wishlist) {
+            // Otherwise send them to the search form.
+            drupal_set_message($this->t('The wish list you requested could not be found.  Perhaps you can try looking for it through the wish list search form below.'));
+            return $this->redirect('uc_wishlist.search');
+
+        }
+        // Display only if the users wishlist is not set to private
+        if (!$wishlist->private) {
+
+            //Set the title to the wishlist title
+            $title = $wishlist->title;
+            $items = uc_wishlist_get_contents($wid);
+
+            if (empty($items)) {
+                //@TODO: add the users name to the output string
+                $output = '<p>There are no products in this wish list.</p>';
+                //return $render;
+            } else {
+                $form = \Drupal::formBuilder()->getForm('Drupal\uc_wishlist\Form\WishlistViewForm', $items, $wid, false);//('uc_wishlist_view_form', $items, $wid, $own);
+                $rendered_wishlistview_form = \Drupal::service('renderer')->render($form);
+            }
+        } else {
+            drupal_set_message($this->t('This users wish list is set to private. You may search for another user\'s wish list below.'));
+            return $this->redirect('uc_wishlist.search');
+        }
+        $render['#theme'] = 'uc_wishlist_view_wishlist';
+        $render['#type'] = 'theme';
+        $render['#title'] = $title;
+        $render['#message'] = $output;
+        $render['#form'] = $rendered_wishlistview_form;
+        $render['#wishlist'] = $wishlist;
+
+        return $render;
+    }
+
+    public function myWishlist()
+    {
+        //setup our render array for all our page variables and configurations
+        $render = array();
+        //get the wishlist id based off of the current users id
+        $wid = uc_wishlist_get_wid();
+        // Attempt to load the wish list.
+        $wishlist = uc_wishlist_load($wid);
+        $output = '';
+        if (!$wishlist) {
+            // Display a message letting them know their list is empty.
+            $title = 'My Wish list';
+            drupal_set_message($this->t("You have not added any products to your wish list. You can add any product from the store to your wish list by clicking the 'Add to wish list' button on the product's page."));
+            $render['#markup'] = 'There are no products on your wish list.';
+            $render['#title'] = $title;
+            return $render;
+        }
+        $title = 'My Wish List';
+        $items = uc_wishlist_get_contents($wid);
+
+        if (empty($items)) {
+            $render['#markup'] = '<p>There are no products in you\'r wish list. You can add products by clicking the Add to wish list button on the product\'s page</p>';
+            return $render;
+        }
+
+        $form = \Drupal::formBuilder()->getForm('Drupal\uc_wishlist\Form\WishlistViewForm', $items, $wid, true);//('uc_wishlist_view_form', $items, $wid, $own);
+        $rendered_wishlistview_form = \Drupal::service('renderer')->render($form);
+        $render['#theme'] = 'uc_wishlist_view_wishlist';
+        $render['#type'] = 'theme';
+        $render['#title'] = $title;
+        $render['#message'] = $output;
+        $render['#form'] = $rendered_wishlistview_form;
+        $render['#wishlist'] = $wishlist;
+        return $render;
+    }
+}
