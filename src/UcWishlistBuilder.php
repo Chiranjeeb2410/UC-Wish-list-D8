61792dffbc5547ce024c1aa2dde256cbc9432d16
chiranjeeb2410 <chiranjeebm2410@gmail.com>
2017-07-11 12:21:02 -0400

    creates wishlist entities

---
+++ src/UcWishlistBuilder.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace Drupal\myentity\Entity\Controller;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityListBuilder;
+use Drupal\Core\Url;
+use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\Query\QueryFactory;
+use Drupal\Core\Routing\RedirectDestinationInterface;
+
+/**
+ * Defines a class to build a listing of wishlist entities.
+ *
+ *@see \Drupal\uc_wishlist\Entity\UcWishlist
+ */
+class UcWishlistBuilder extends EntityListBuilder {
+
+  /**
+   * The entity query factory.
+   *
+   * @var \Drupal\Core\Entity\Query\QueryFactory
+   */
+  protected $queryFactory;
+
+  /**
+   * The date formatter service.
+   *
+   * @var \Drupal\Core\Datetime\DateFormatterInterface
+   */
+  protected $dateFormatter;
+
+  /**
+   * The redirect destination service.
+   *
+   * @var \Drupal\Core\Routing\RedirectDestinationInterface
+   */
+  protected $redirectDestination;
+
+  /**
+   * Constructs a new UcWishlistListBuilder object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type definition.
+   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
+   *   The entity storage class.
+   * @param \Drupal\Core\Entity\Query\QueryFactory $query_factory
+   *   The entity query factory.
+   * @param \Drupal\Core\Datetime\DateFormatterInterface $date_formatter
+   *   The date formatter service.
+   * @param \Drupal\Core\Routing\RedirectDestinationInterface $redirect_destination
+   *   The redirect destination service.
+   */
+  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, QueryFactory $query_factory, DateFormatterInterface $date_formatter, RedirectDestinationInterface $redirect_destination) {
+    parent::__construct($entity_type, $storage);
+    $this->queryFactory = $query_factory;
+    $this->dateFormatter = $date_formatter;
+    $this->redirectDestination = $redirect_destination;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
+    return new static(
+      $entity_type,
+      $container->get('entity.manager')->getStorage($entity_type->id()),
+      $container->get('entity.query'),
+      $container->get('date.formatter'),
+      $container->get('redirect.destination')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function load() {
+    $entity_query = $this->queryFactory->get('uc_wishlist');
+    $entity_query->pager(50);
+    $header = $this->buildHeader();
+    $entity_query->tableSort($header);
+    $uids = $entity_query->execute();
+    return $this->storage->loadMultiple($uids);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildHeader() {
+    $header = [
+      'title' => [
+        'data' => $this->t('Title'),
+        'field' => 'title',
+        'specifier' => 'title',
+      ],
+      'expiration' => [
+        'data' => $this->t('Expiration date'),
+        'field' => 'expiration',
+        'specifier' => 'expiration',
+        'class' => [RESPONSIVE_PRIORITY_LOW],
+      ],
+      'address' => [
+        'data' => $this->t('Address'),
+        'field' => 'address',
+        'specifier' => 'address',
+        'class' => [RESPONSIVE_PRIORITY_LOW],
+      ],
+      'private' => [
+        'data' => $this->t('Private'),
+        'field' => 'private',
+        'specifier' => 'private',
+        'class' => [RESPONSIVE_PRIORITY_LOW],
+      ],
+      'description' => [
+        'data' => $this->t('Description'),
+        'field' => 'description',
+        'specifier' => 'description',
+        'class' => [RESPONSIVE_PRIORITY_LOW],
+      ],
+    ];
+    return $header + parent::buildHeader();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildRow(EntityInterface $entity) {
+    $row['title'] = $entity->getTitle();
+    $row['expiration'] = $entity->getExpirationTime();
+    $row['address'] = $entity->getAddress();
+    $row['private'] = $entity->getPrivate();
+    $row['description'] = $entity->getDescription();
+
+    return $row + parent::buildRow($entity);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOperations(EntityInterface $entity) {
+    $operations = parent::getOperations($entity);
+    if (isset($operations['edit'])) {
+      $destination = $this->redirectDestination->getAsArray();
+      $operations['edit']['query'] = $destination;
+    }
+    return $operations;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render() {
+    $build = parent::render();
+    $build['table']['#empty'] = $this->t('No wish lists found.');
+    return $build;
+  }
+
+}
